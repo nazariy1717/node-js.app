262b9416166a5d3b321a701fb955bb2f0284eaee
nazariy17 <nazariy2410@gmail.com>
2018-02-24 14:03:57 +0200

    http start

---
+++ lesson_3/enviropment/main.js
@@ -0,0 +1,12 @@
+
+require('./test.js');
+
+console.log('main js');
+
+
+console.log('directory name: ', __dirname);
+console.log('file name: ', __filename);
+
+
+console.log(require.cache); // вдастивість cache містить в собі вміст всіх підключених модулів
+
